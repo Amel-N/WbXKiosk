69bd2f3b8beaaa1575161176b1db75795a4854f9
Amel-N <145393399+Amel-N@users.noreply.github.com>
2024-11-01 15:04:49 -0400

    Initial commit of WebexKiosk project

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber: 'amel.nukicic@ignitetechnology.com',
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber, name: 'Reception' },
+        { url: this.dialNumber, name: 'Shipping' },
+        { url: this.dialNumber, name: 'about' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
